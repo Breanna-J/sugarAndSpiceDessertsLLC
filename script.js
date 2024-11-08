b02df10f6b97f286f8fcdb011e3ff311718bf432
Breanna-J <kalebandbre@gmail.com>
2024-11-07 23:33:21 -0600

    adding and styling form

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function toggleHamburgerMenu(){
     document.getElementById("menu").classList.toggle
     ("active");
 }
+
+const form = document.querySelector('form');
+
+function sendEmail() {
+    
+}
